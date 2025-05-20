e774750a88e96c79546be42b91f3e468e75a17ec
alexx-kuzmenk0 <alexkuzmenko19082@gmail.com>
2025-05-20 11:55:19 +0200

    add app news

--- djangoco/main/views.py
+++ djangoco/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def index(request):
+    data = {
+        'title': 'Home Page',
+        }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
